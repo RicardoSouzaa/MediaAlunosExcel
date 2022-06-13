9cbc6489616388a1c62678265b3259528b403181
RIcardo Souza <ryszard696@yahoo.com.br>
2022-06-13 16:23:56 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace MediaAlunosExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+                SalvarPlanilha();
+                excelApp.Quit();
+                this.Close();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblNota1 = new System.Windows.Forms.Label();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.lblNota2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.cbNota1 = new System.Windows.Forms.ComboBox();
+            this.cbNota2 = new System.Windows.Forms.ComboBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(13, 13);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(47, 15);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "NOME:";
+            // 
+            // lblNota1
+            // 
+            this.lblNota1.AutoSize = true;
+            this.lblNota1.Location = new System.Drawing.Point(13, 70);
+            this.lblNota1.Name = "lblNota1";
+            this.lblNota1.Size = new System.Drawing.Size(46, 15);
+            this.lblNota1.TabIndex = 1;
+            this.lblNota1.Text = "NOTA1";
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Location = new System.Drawing.Point(13, 127);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(45, 15);
+            this.lblMedia.TabIndex = 2;
+            this.lblMedia.Text = "MÉDIA";
+            // 
+            // lblNota2
+            // 
+            this.lblNota2.AutoSize = true;
+            this.lblNota2.Location = new System.Drawing.Point(205, 70);
+            this.lblNota2.Name = "lblNota2";
+            this.lblNota2.Size = new System.Drawing.Size(46, 15);
+            this.lblNota2.TabIndex = 1;
+            this.lblNota2.Text = "NOTA2";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(67, 7);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(309, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Location = new System.Drawing.Point(67, 122);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.Size = new System.Drawing.Size(110, 20);
+            this.txtMedia.TabIndex = 4;
+            // 
+            // cbNota1
+            // 
+            this.cbNota1.FormattingEnabled = true;
+            this.cbNota1.Location = new System.Drawing.Point(67, 67);
+            this.cbNota1.Name = "cbNota1";
+            this.cbNota1.Size = new System.Drawing.Size(110, 21);
+            this.cbNota1.TabIndex = 5;
+            // 
+            // cbNota2
+            // 
+            this.cbNota2.FormattingEnabled = true;
+            this.cbNota2.Location = new System.Drawing.Point(266, 67);
+            this.cbNota2.Name = "cbNota2";
+            this.cbNota2.Size = new System.Drawing.Size(110, 21);
+            this.cbNota2.TabIndex = 6;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(219, 119);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(157, 23);
+            this.btnOk.TabIndex = 7;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 181);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.cbNota2);
+            this.Controls.Add(this.cbNota1);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.lblNota2);
+            this.Controls.Add(this.lblNota1);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Média Excel";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblNota1;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Label lblNota2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtMedia;
+        private System.Windows.Forms.ComboBox cbNota1;
+        private System.Windows.Forms.ComboBox cbNota2;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace MediaAlunosExcel
+{
+    public partial class Form1 : Form
+    {
+        private string aluno, aprovado, livro;
+        private int freq1, freq2;
+        private float media;
+        private Excel.Application excelApp;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtNome.Text = null;
+
+            cbNota1.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbNota2.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            cbNota1.Items.Add("");
+            cbNota2.Items.Add("");
+
+            int aval;
+            for (aval = 0; aval <= 20; aval++)
+            {
+                cbNota1.Items.Add(aval);
+                cbNota2.Items.Add(aval);
+            }
+
+            cbNota1.Text = null;
+            cbNota2.Text = null;
+
+            txtMedia.Text = null;
+            txtMedia.ReadOnly = true;
+
+            IniciarExcel();
+
+            if (VerificarPlanilha() == false)
+            {
+                CriarPlanilha();
+            }
+            else
+            {
+                AbrirPlanilha();
+            }
+        }
+
+        private void IniciarExcel()
+        {
+            excelApp = new Excel.Application();
+            excelApp.Visible = true;
+        }
+
+        private bool VerificarPlanilha()
+        {
+            livro = Application.StartupPath + "/alunos.xlsx";
+            if (File.Exists(livro))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void CriarPlanilha()
+        {
+            excelApp.Workbooks.Add();
+            excelApp.Sheets["Planilha1"].Select();
+
+            excelApp.Range["A1"].Value = "Nome";
+            excelApp.Range["B1"].Value = "Nota1";
+            excelApp.Range["C1"].Value = "Nota2";
+            excelApp.Range["D1"].Value = "Média";
+            excelApp.Range["E1"].Value = "Situação";
+
+            FormatarPlanilha();
+        }
+
+        private void FormatarPlanilha()
+        {
+            excelApp.Range["A1:E1"].Font.Bold = true;
+            excelApp.Range["A1:E1"].Interior.Color = Excel.Constants.xlGray50;
+            excelApp.Range["A1:E1"].HorizontalAlignment = Excel.Constants.xlCenter;
+
+            excelApp.Range["A:A"].ColumnWidth = 30;
+            excelApp.Range["B:B"].ColumnWidth = 10;
+            excelApp.Range["C:C"].ColumnWidth = 10;
+            excelApp.Range["D:D"].ColumnWidth = 10;
+            excelApp.Range["E:E"].ColumnWidth = 20;
+
+            if (VerificarPlanilha() == false)
+            {
+                SalvarPlanilhaComo();
+            }
+            else
+            {
+                SalvarPlanilha();
+            }
+        }
+
+        private void SalvarPlanilhaComo()
+        {
+            excelApp.ActiveWorkbook.SaveAs(livro);
+        }
+
+        private void SalvarPlanilha()
+        {
+            excelApp.ActiveWorkbook.Save();
+        }
+
+        private void AbrirPlanilha()
+        {
+            try
+            {
+                excelApp.Workbooks.Open(livro);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Falha : " + ex.Message);
+            }
+        }
+
+        private bool ValidarDados()
+        {
+            try
+            {
+                if (txtNome.Text.Length < 2)
+                {
+                    MessageBox.Show("Introduza o nome do Aluno");
+                    txtNome.SelectAll();
+                    txtNome.Focus();
+                    return false;
+                }
+                if (cbNota1.Text.Length == 0)
+                {
+                    MessageBox.Show("Selecione a Nota 1");
+                    cbNota1.Focus();
+                    return false;
+                }
+                if (cbNota2.Text.Length == 0)
+                {
+                    MessageBox.Show("Selecione a Nota 2");
+                    cbNota2.Focus();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Falha: " + ex.Message);
+            }
+            return true;
+        }
+
+        private void CalcularMedia()
+        {
+            aluno = txtNome.Text;
+            freq1 = int.Parse(cbNota1.Text);
+            freq2 = int.Parse(cbNota2.Text);
+            media = (freq1 + freq2) / 2;
+
+            if (freq1 < 8 && freq2 < 8)
+            {
+                aprovado = "Reprovado";
+            }
+            else if (freq1 < 8 && freq2 >= 8)
+            {
+                aprovado = "Repetir Nota1";
+            }
+            else if (freq2 < 8 && freq1 >= 8)
+            {
+                aprovado = "Repetir Nota 2";
+            }
+            else if (freq1 >= 8 && freq2 >= 8)
+            {
+                if (media >= 9.5)
+                {
+                    aprovado = "Aprovado";
+                }
+                else
+                {
+                    aprovado = "Prova oral";
+                }
+            }
+
+            txtMedia.Text = ($"{media} ({aprovado})");
+            ExportarExcel();
+        }
+
+        private void ExportarExcel()
+        {
+            excelApp.Sheets["Planilha1"].Select();
+
+            int linhaExcel = 2;
+            bool valor = true;
+            while (valor == true)
+            {
+                if (excelApp.Range["A" + linhaExcel].Value != null)
+                {
+                    valor = true;
+                    linhaExcel = linhaExcel + 1;
+                }
+                else
+                {
+                    valor = false;
+                }
+            }
+
+            excelApp.Range["A" + linhaExcel].Value = aluno;
+            excelApp.Range["B" + linhaExcel].Value = freq1;
+            excelApp.Range["C" + linhaExcel].Value = freq2;
+            excelApp.Range["D" + linhaExcel].Value = media;
+            excelApp.Range["E" + linhaExcel].Value = aprovado;
+            FormatarPlanilha();
+            Cores();
+        }
+
+        private void Cores()
+        {
+            excelApp.Sheets["Planilha1"].Select();
+
+            int linhaExcel = 2;
+            bool valor = true;
+            string sit;
+            while (valor == true)
+            {
+                if (excelApp.Range["E" + linhaExcel].Value != null)
+                {
+                    sit = excelApp.Range["E" + linhaExcel].Value;
+
+                    switch (sit)
+                    {
+                        case "Aprovado":
+                            excelApp.Range["A" + linhaExcel + ":" + "E" + linhaExcel].Interior.Color = Color.FromArgb(0, 255, 0);
+                            excelApp.Range["A" + linhaExcel + ":" + "E" + linhaExcel].Font.Color = Color.FromArgb(0, 0, 0);
+                            break;
+
+                        case "Reprovado":
+                            excelApp.Range["A" + linhaExcel + ":" + "E" + linhaExcel].Interior.Color = Color.FromArgb(255, 0, 0);
+                            excelApp.Range["A" + linhaExcel + ":" + "E" + linhaExcel].Font.Color = Color.FromArgb(255, 255, 255);
+                            break;
+
+                        case "Prova Oral":
+                            excelApp.Range["A" + linhaExcel + ":" + "E" + linhaExcel].Interior.Color = Color.FromArgb(255, 255, 0);
+                            excelApp.Range["A" + linhaExcel + ":" + "E" + linhaExcel].Font.Color = Color.FromArgb(0, 0, 0);
+                            break;
+
+                        default:
+                            excelApp.Range["A" + linhaExcel + ":" + "E" + linhaExcel].Interior.Color = Excel.Constants.xlNone;
+                            excelApp.Range["A" + linhaExcel + ":" + "E" + linhaExcel].Font.Color = Color.FromArgb(0, 0, 0);
+                            break;
+                    }
+                    valor = true;
+                    linhaExcel = linhaExcel + 1;
+                }
+                else
+                {
+                    valor = false;
+                }
+            }
+            SalvarPlanilha();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (ValidarDados() == true)
+            {
+                CalcularMedia();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SalvarPlanilha();
+            excelApp.Quit();
+        }
+    }
+}
